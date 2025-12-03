cc383dca75bf493b8a2f1bd60c0eda2acc4ee790
Bartinho007 <korinfart04@yandex.ru>
2025-12-04 00:25:54 +0300

    db fixes

--- db.py
+++ db.py
@@ -38,9 +38,11 @@ class Order(Base):
 
 # Инициализация БД
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///rent.db")
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
-Base.metadata.create_all(engine)
-SessionLocal = sessionmaker(bind=engine)
+if "sqlite" in DATABASE_URL:
+    engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+else:
+    engine = create_engine(DATABASE_URL)
+
 
 
 @contextmanager
